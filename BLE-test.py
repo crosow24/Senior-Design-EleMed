d77910bfa0f52a6264254dfc6f1757a52058a3e2
crosow24 <74068105+crosow24@users.noreply.github.com>
2023-11-19 14:43:26 -0500

    Update BLE-test.py

--- BLE-test.py
+++ BLE-test.py
@@ -1,44 +1,62 @@
 from BLE_CEEO import Yell, Listen
 import time
+import TwilioLibrary
 
-def peripheral(name):
+def peripheral(name): 
+    global ssid = "Unknown"
+    global password = "Unknown"
     try:
         p = Yell(name, verbose = True)
         if p.connect_up():
             print('P connected')
             time.sleep(2)
-            payload = 'test'
-            p.send(payload)
-            while True:
+            payload = 'test' 
+            p.send(payload) 
+            while (ssid == "Unknown" or password == "Unknown"):
                 if p.is_any:
                     print(p.read())
+                    if ssid == "Unknown":
+                        ssid = p.read()
+                    elif password == "Unknown":
+                        if p.read() == "None" or p.read == "NA" 0r p.read == "N/A" or p.read == "none":
+                            password = ""
+                            end = True
+                        else:
+                            password = p.read()
+                            end = True
                 if not p.is_connected:
                     print('lost connection')
                     break
+                if end:
+                    global wifi = {'ssid':ssid,'pass':password}
+                    TwilioLibrary.connect_wifi(wifi)
+                    #test connect to AirTable API
+                    #if fails, reset ssid and password to unknown and have user try again
                 time.sleep(1)
+            
     except Exception as e:
         print(e)
     finally:
         p.disconnect()
         print('closing up')
-def central(name):
-    try:
+def central(name):   
+    try:   
         L = Listen(name, verbose = True)
         if L.connect_up():
             print('L connected')
             while L.is_connected:
-                time.sleep(4)
-                if L.is_any:
-                    reply = L.read()
-                    print(reply) #seems to stop at 80 characteres
-                    L.send(reply[:20])  #seems to stop around 20 characters
+                time.sleep(1)
+                L.send(global wifi)  #seems to stop around 20 characters
     except Exception as e:
         print(e)
     finally:
         L.disconnect()
         print('closing up')
+        
 
-peripheral("Corey")
+peripheral("EleMed Pad")
+central("EleMed Pad")
+    
 
 
 '''
