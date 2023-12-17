6bf8d5ed2ef1b7561b982942275e030513f3b809
crosow24 <74068105+crosow24@users.noreply.github.com>
2023-12-17 18:00:14 -0500

    Update TwilioLibrary.py

--- TwilioLibrary.py
+++ TwilioLibrary.py
@@ -5,9 +5,20 @@ import ubinascii
 import urequests as requests
 import umail
 import mqtt
+import json
+
 from keys import account_sid
 from keys import auth_token
 from keys import email_password
+from keys import base_id
+from keys import AIRTABLE_URL
+from keys import AIRTABLE_TOKEN
+from keys import history_url
+from keys import User_Info_url
+from keys import sorted_history_url
+from keys import headers as at_headers
+from keys import Tufts_Wireless as wifi
+
 
 def whenCalled(topic, msg):
     global wcdata
@@ -67,3 +78,117 @@ def phone_call(recipient):
     #fred.connect()
     #fred.set_callback(whenCalled)
     #fred.publish('crosow24/feeds/temp',msg)
+def getUserData():
+    response = requests.get(User_Info_url, headers = at_headers)
+    print(response.status_code)
+    data = response.json()
+    return data
+def postData(col_input1,col_input2,col_input3):
+    #col_name = "Adherence_Status"
+    #col_input = "missed" #eventually will
+    data = {
+        "records": [
+            {
+                "fields": {
+                    'Date' : col_input1, 'Time' : col_input2, 'Adherence_Status' : col_input3,
+                }
+            }
+        ]
+    }
+    response = requests.request("POST", history_url, headers=at_headers, data=json.dumps(data))
+    print(response)
+def patchData(field, col_input1, recordID):
+    data = {
+        "records": [
+            {
+                "id": recordID,
+                "fields": {
+                    field : col_input1,
+                }
+            }
+        ]
+    }
+    response = requests.request("PATCH", history_url, headers=at_headers, data=json.dumps(data))
+    code = response.json()
+    print(code)
+def patchDate(col_input1, recordID):
+    data = {
+        "records": [
+            {
+                "id": recordID,
+                "fields": {
+                    "Date" : col_input1,
+                }
+            }
+        ]
+    }
+    response = requests.request("PATCH", history_url, headers=at_headers, data=json.dumps(data))
+    code = response.json()
+    print(code)
+def patchTime(col_input1, recordID):
+    data = {
+        "records": [
+            {
+                "id": recordID,
+                "fields": {
+                    "Time" : col_input1,
+                }
+            }
+        ]
+    }
+    response = requests.request("PATCH", history_url, headers=at_headers, data=json.dumps(data))
+    code = response.json()
+    print(code)
+def patchStatus(col_input1, recordID):
+    data = {
+        "records": [
+            {
+                "id": recordID,
+                "fields": {
+                    "Adherence_Status" : col_input1,
+                }
+            }
+        ]
+    }
+    response = requests.request("PATCH", history_url, headers=at_headers, data=json.dumps(data))
+    code = response.json()
+    print(code)
+def AdheranceStatus():
+    response = requests.get(sorted_history_url, headers = at_headers)
+    data = response.json()
+    length = len(data['records'])
+    data=data['records'][length-1]['fields']['Adherence_Status']
+    return data
+def TimeTaken():
+    response = requests.get(sorted_history_url, headers = at_headers)
+    data = response.json()
+    length = len(data['records'])
+    data=data['records'][length-1]['fields']['Time']
+    return data
+def DateTaken():
+    response = requests.get(sorted_history_url, headers = at_headers)
+    data = response.json()
+    length = len(data['records'])
+    data=data['records'][length-1]['fields']['Date']
+def mostRecentID():
+    response = requests.get(sorted_history_url, headers = at_headers)
+    data = response.json()
+    length = len(data['records'])
+    data=data['records'][length-1]['id']
+    return data
+def parseData(data, column):
+    #pull data apart
+    data=data['records'][0]['fields'][column]
+    return data
+
+time_headers = {
+    'accept': 'application/json',
+}
+def getTime():
+    response = requests.get('https://timeapi.io/api/Time/current/coordinate?latitude=38.9&longitude=-77.03', headers=time_headers)
+    data=response.json()
+    return (data['time'])
+def getDate():
+    response = requests.get('https://timeapi.io/api/Time/current/coordinate?latitude=38.9&longitude=-77.03', headers=time_headers)
+    data=response.json()
+    return (data['date'])
