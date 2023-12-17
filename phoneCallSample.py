ca9f01ba1485ae74036fa20ced54ce3353732ee9
crosow24 <74068105+crosow24@users.noreply.github.com>
2023-12-17 18:08:40 -0500

    Update phoneCallSample.py

--- phoneCallSample.py
+++ phoneCallSample.py
@@ -4,14 +4,14 @@ from twilio.rest import Client
 
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
-account_sid = 'AC38c976bd0b292236dbaa00a56bf2d1c1'
-auth_token = '145eec63e9347e8de12760dbceb809de'
+account_sid = 'REDACTED'
+auth_token = 'REDACTED'
 client = Client(account_sid, auth_token)
 
 call = client.calls.create(
                         url='http://demo.twilio.com/docs/voice.xml',
-                        to='+12037278473',
-                        from_='+18337780524'
+                        to='+REDACTED',
+                        from_='+REDACTED'
                     )
 
 print(call.sid)
